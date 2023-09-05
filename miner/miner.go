a0bb2b17748520327749a27c7b433c50f76aa203
CYBER STORM <43612782+formysister@users.noreply.github.com>
2023-09-06 06:35:22 +0900

    Update miner.go

--- miner/miner.go
+++ miner/miner.go
@@ -10,10 +10,6 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 // Package miner implements Ethereum block creation and mining.
 package miner
 
